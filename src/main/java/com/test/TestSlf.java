ffcc2fefcacdd02279584c51f6a3a8472bfbf024
272417707 <ly871336>
2018-01-19 16:53:53 +0800

    Initial commit

---
+++ src/main/java/com/test/TestSlf.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestSlf {
+    final Logger logger = LoggerFactory.getLogger(TestSlf.class);
+
+    private void test() {
+        logger.info("这是一条日志信息 - {}", "我是参数");
+    }
+
+    public static void main(String[] args) {
+        TestSlf testSlf = new TestSlf();
+        testSlf.test();
+
+        System.out.println("Hello World!");
+    }
+}

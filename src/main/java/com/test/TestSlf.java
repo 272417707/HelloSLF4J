9c16674b87f8546481715eb09111aef7799e0e86
272417707 <ly871336>
2018-01-19 17:24:39 +0800

    第一次更新

--- src/main/java/com/test/TestSlf.java
+++ src/main/java/com/test/TestSlf.java
@@ -8,7 +8,6 @@ public class TestSlf {
 
     private void test() {
         logger.info("这是一条日志信息 - {}", "我是参数");
-        logger.info("第一次更新");
     }
 
     public static void main(String[] args) {

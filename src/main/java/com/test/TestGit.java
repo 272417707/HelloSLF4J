741de1d7a016a8a88653522aed5ac4b2603426fe
272417707 <ly871336>
2018-01-23 16:15:40 +0800

    第儿次更新

--- src/main/java/com/test/TestGit.java
+++ src/main/java/com/test/TestGit.java
@@ -7,5 +7,9 @@ public class TestGit {
 
     TestGit tg = new TestGit();
     TestGit tg2 = new TestGit();
+    public void testGit(){
+        System.out.print("1");
+        System.out.print("1");
+    }
 
 }
